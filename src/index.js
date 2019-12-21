d7f859ada84966717236140da340b6b206aaf50e
Ryan <ryan@mmnto.io>
2019-12-21 12:45:04 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,116 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ImageComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImage: null
+    }
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  handleClick(animal) {
+    this.setState({ currentImage: animal });
+  }
+
+  render() {
+    let currentImage = null;
+    if (this.state.currentImage) {
+      let currentImageURL;
+      if (this.state.currentImage == 'dog') {
+        currentImageURL = 'dog.jpg';
+      } else {
+        currentImageURL = 'cat.jpg';
+      }
+      currentImage = <img src={currentImageURL} />;
+    }
+
+    return (
+      <div className="imageComponent">
+        <div className="buttonBox">
+          <button className="btn btn-dark" onClick={ () => this.handleClick('dog')}>Dog</button>
+          <button className="btn btn-light" onClick={ () => this.handleClick('Cat')}>Cat</button>
+        </div>
+        <div className="imageContainer">
+          {currentImage}
+        </div>
+      </div>
+    );
+  }
+}
+
+class NameComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      finalName: '',
+      name: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ finalName: this.state.name, name: '' });
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="nameComponent">
+        <p>{ 'Hello ' + this.state.finalName + '!' }</p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input className="form-control" type="text" value={ this.state.name } onChange={ this.handleChange } />
+          </label>
+          <input className="btn btn-success" type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+class TabUI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTab: 0
+    }
+  }
+
+  handleClick(i) {
+    this.setState({ currentTab: i});
+  }
+
+  render() {
+    let currentView;
+    if (this.state.currentTab == 0) {
+      currentView = <NameComponent />;
+    } else {
+      currentView = <ImageComponent />;
+    }
+    return (
+      <div>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a className={"nav-link " + (this.state.currentTab == 0 ? "active" : "")} onClick={ () => this.handleClick(0) } href="#">Name</a>
+          </li>
+          <li className="nav-item">
+            <a className={"nav-link " + (this.state.currentTab == 1 ? "active" : "")} onClick={ () => this.handleClick(1) }href="#">Image</a>
+          </li>
+        </ul>
+        { currentView }
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<TabUI />, document.getElementById('root'));
